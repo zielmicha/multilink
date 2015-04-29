2f13bc77d5d6bae38477c1480890941b0453f58a
Michał Zieliński <michal@zielinscy.org.pl>
2015-04-29 23:06:00 +0200

    app: provide-stream

--- app/app.cpp
+++ app/app.cpp
@@ -1,16 +1,41 @@
 #include "multilink.h"
 #include <iostream>
 #include "rpc.h"
+#include "ioutil.h"
+
+class Server {
+    Reactor& reactor;
+    std::unordered_map<int, FD*> unused_stream_fds;
+
+public:
+    Server(Reactor& reactor): reactor(reactor) {}
+
+    void callback(std::shared_ptr<RPCStream> stream,
+                  Json message) {
+        std::string type = message["type"].string_value();
+        std::cerr << "recv " << message.dump() << std::endl;
+
+        if(type == "provide-stream") {
+            int num = message["num"].int_value();
+            FD* fd = stream->abandon();
+            unused_stream_fds[num] = fd;
+            ioutil::write(fd, ByteString::copy_from("ok\n"));
+        } else {
+            std::cerr << "bad type " << type << std::endl;
+        }
+    }
+};
 
 int main() {
     Reactor reactor;
 
+    Server server {reactor};
     auto callback = [&](std::shared_ptr<RPCStream> stream,
                         Json message) {
-        std::cerr << "recv " << message.dump() << std::endl;
+        server.callback(stream, message);
     };
-
-    auto server = RPCServer::create(reactor, "app.sock", callback);
+    auto rpcserver = RPCServer::create(reactor, "app.sock",
+                                       callback);
 
     reactor.run();
 }
--- client/app.py
+++ client/app.py
@@ -2,7 +2,23 @@ import socket
 import struct
 import json
 
-s = socket.socket(socket.AF_UNIX)
-s.connect('build/app.sock')
-msg = json.dumps({'hello': 'world'})
-s.sendall(struct.pack('I', len(msg)) + msg)
+class Connection(object):
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_UNIX)
+        self.sock.connect('build/app.sock')
+        self.file = self.sock.makefile('r+')
+
+    def send(self, msg):
+        msg = json.dumps(msg)
+        self.sock.sendall(struct.pack('I', len(msg)) + msg)
+
+    def provide_stream(self, num):
+        self.send({'type': 'provide-stream', 'num': num})
+        resp = self.file.read(3)
+        if resp != 'ok\n':
+            raise IOError('bad response to provide-stream')
+        return self.file
+
+conn = Connection()
+f = conn.provide_stream(0)
+print f
