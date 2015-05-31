9c2b51e682a019afd9869493e3be403dbbb77a33
Michał Zieliński <michal@zielinscy.org.pl>
2015-05-31 12:45:43 +0200

    print_stack_trace function

--- libreactor/logging.cpp
+++ libreactor/logging.cpp
@@ -13,13 +13,22 @@ static void handler(int sig) {
   void *array[10];
   size_t size;
 
-  // get void*'s for all entries on the stack
   size = backtrace(array, 10);
 
   // print out all the frames to stderr
   fprintf(stderr, "Error: signal %d:\n", sig);
   backtrace_symbols_fd(array, size, STDERR_FILENO);
   exit(1);
+}
+
+void print_stack_trace() {
+  void *array[10];
+  size_t size;
+
+  size = backtrace(array, 10);
+
+  fprintf(stderr, "Print stack trace:\n");
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
 }
 
 void setup_crash_handlers() {
