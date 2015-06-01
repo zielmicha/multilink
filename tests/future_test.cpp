9ca34849e185c91eb534f02c57a5d28082f3edcf
Michał Zieliński <michal@zielinscy.org.pl>
2015-06-01 22:59:48 +0200

    'then<T>' can now be replaced with 'then'

---
+++ tests/future_test.cpp
@@ -0,0 +1,8 @@
+#include "future.h"
+
+int main() {
+    Future<int> f = Future<int>::make_immediate(0);
+
+    Future<int> g = f.then([](int v) { return v + 1; });
+    Future<int> h = f.then([](int v) { return Future<int>::make_immediate(v + 1); });
+}
